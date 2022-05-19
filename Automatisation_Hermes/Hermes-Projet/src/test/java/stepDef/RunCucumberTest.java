eba992c3b41a1de6d73852dbcf9b6ac349b9b227
AbdelrazakAltounji <Abdelrazak.alt@hotmail.com>
2022-05-19 16:45:33 +0200

    Update RunCucumberTest.java

--- Automatisation_Hermes/Hermes-Projet/src/test/java/stepDef/RunCucumberTest.java
+++ Automatisation_Hermes/Hermes-Projet/src/test/java/stepDef/RunCucumberTest.java
@@ -4,12 +4,12 @@ import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-
+import io.cucumber.junit.CucumberOptions;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 //@RunWith(Cucumber.class)
-//@CucumberOptions(features="src/test/resources/cucumber", glue= {"StepDefinitions"},monochrome=true)
+@CucumberOptions(features="Hermes-Projet/Automatisation_Hermes", glue= {"StepDefinitions"},monochrome=true)
 @IncludeEngines("cucumber")
 @SelectClasspathResource("cucumber")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
