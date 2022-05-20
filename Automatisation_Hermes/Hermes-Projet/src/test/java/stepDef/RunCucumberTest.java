55cbc2d87aa412a47e73932c418e8608996d4146
AbdelrazakAltounji <Abdelrazak.alt@hotmail.com>
2022-05-20 11:05:44 +0200

    Update RunCucumberTest.java

--- Automatisation_Hermes/Hermes-Projet/src/test/java/stepDef/RunCucumberTest.java
+++ Automatisation_Hermes/Hermes-Projet/src/test/java/stepDef/RunCucumberTest.java
@@ -1,17 +1,18 @@
 package stepDef;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
-import io.cucumber.junit.CucumberOptions;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+//import org.junit.platform.suite.api.ConfigurationParameter;
+//import org.junit.platform.suite.api.IncludeEngines;
+//import org.junit.platform.suite.api.SelectClasspathResource;
+//import org.junit.platform.suite.api.Suite;
+//import io.cucumber.junit.CucumberOptions;
+//import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import io.cucumber.testng.*;
 
-@Suite
-//@RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/cucumber", glue= {"StepDefinitions"},monochrome=true, plugin = { "pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"})
-@IncludeEngines("cucumber")
-@SelectClasspathResource("cucumber")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-public class RunCucumberTest {
+
+@CucumberOptions(features="src/test/resources/cucumber", glue= {"stepDef"},
+monochrome=true, plugin= {"pretty","junit:target/JunitReports/reports.xml","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+		"json:target/JsonReports/reports.json",
+		"html:target/HtmlReports/reports.html"})
+
+public class RunCucumberTest extends AbstractTestNGCucumberTests{
 }
